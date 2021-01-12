9372f1e55b15e581e954fa525ca3bb90fc80cd53
kati.mani <tesgit279@gmail.com>
2021-01-12 11:54:47 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,30 +4,47 @@ class UsersController < ApplicationController
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
-
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      flash[:success] = 'ユーザーを登録しました。'
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_name_params)
+      flash[:success] = '名前を変更しました。'
+      redirect_to @user
+    else
+      flash[:danger] = '名前の変更に失敗しました。'
+      render :edit
+    end
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def user_name_params
+    params.require(:user).permit(:name)
+  end
 end
+
