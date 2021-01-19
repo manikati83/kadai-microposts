91e8a832c889d318813a1b3bb5aab89543888700
kati.mani <tesgit279@gmail.com>
2021-01-19 13:27:22 +0000

    favorite, unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,17 @@ class User < ApplicationRecord
         uniqueness: { case_sensitive: false }
     has_secure_password
     
+    #一対多の関係であることを指定
     has_many :microposts
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    
+    has_many :favorites
+    has_many :favorite_microposts, through: :favorites, source: :micropost
+    has_many :favorite_users, through: :favorites, source: :user
+    
     
     def follow(other_user)
         unless self == other_user
@@ -30,4 +36,21 @@ class User < ApplicationRecord
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
     end
+    
+    
+    def addfavorite(other_micropost)
+      
+        self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+      
+    end
+    
+    def unfavorite(other_micropost)
+        favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+        favorite.destroy if favorite
+    end
+    
+    def favorite?(other_micropost)
+        self.favorite_microposts.include?(other_micropost)
+    end
+    
 end
